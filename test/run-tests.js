9f7fba467c0a056521efb37ef58b224216bf896f
ronald (MacBookie Pro) <ronald@orangewise.com>
2017-12-24 11:29:03 +0100

    pass cwd

--- test/run-tests.js
+++ test/run-tests.js
@@ -4,7 +4,7 @@ var join = require('path').join
 var fs = require('fs')
 
 test('scripts without errors', function (t) {
-  t.plan(6)
+  t.plan(8)
   run.scripts({ c: join(__dirname, 'fixtures', 'scripts.yaml'), v: false }, function (e, r) {
     t.equal(e, null, 'should be empty')
     t.equal(r, 0, 'exit code should be 0')
@@ -16,6 +16,11 @@ test('scripts without errors', function (t) {
     t.equal(log, 'Test\nFOO: BARTHREE\nArguments: foo3\n', 'log 3 looks good')
     log = fs.readFileSync(join(process.cwd(), 'log', 'test4.1.log')).toString()
     t.equal(log, 'Test\nFOO: global bar\nArguments: foo4\n', 'log 4 looks good')
+    log = fs.readFileSync(join(process.cwd(), 'log', 'test5.1.log')).toString()
+    t.equal(log, '/Users/ronald/node/npm/run-forrest-run\n', 'log 5 looks good')
+    log = fs.readFileSync(join(process.cwd(), 'log', 'test6.1.log')).toString()
+    var expected = `${join(process.cwd(), 'test')}\n`
+    t.equal(log, expected, 'log 5 looks good')
   })
 })
 
